bbe18f75e9a8103f14f4cbc60325c8038f6d2689
allinthemind66 <r.nilon92@gmail.com>
2018-04-04 17:44:20 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,14 @@
 // Code SimplerComponentHere Here
+import React from 'react'
+
+const SimplerComponent = (props) => {
+
+  // let handleClick = (e) => {
+  //   e.target.innerText === "I am just happy" ? e.target.innerText = "Now Im Sad" : e.target.innerText = "I am just happy"
+  // }
+
+    return <div onClick={props.handleClick}>I am just happy</div>
+
+}
+
+export default SimplerComponent
