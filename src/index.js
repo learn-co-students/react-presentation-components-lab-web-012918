bfbfb5d59cd9df5f7bd9c58979773d001d7a909d
aalexander3 <aralx73@gmail.com>
2018-04-04 16:04:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+let counter = 0
+
+const beHappy = (e) => {
+  let colors = ['orange', 'yellow', 'red', 'green', 'blue', 'magenta']
+
+  document.body.style.background = colors[counter]
+  counter++;
+  (counter === colors.length) ? counter = 0 : null
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={beHappy} />
   </div>,
   document.getElementById('root')
 );
